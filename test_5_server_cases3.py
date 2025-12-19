03a212fadd3366a893a2fdd29fa3f2b3fdf4d69e
Kostas Pikounis <kostas.pikounis@inlecomsystems.com>
2025-12-19 14:13:33 +0200

    minor updates for server scripts

--- test_5_server_cases3.py
+++ test_5_server_cases3.py
@@ -144,7 +144,7 @@ def search_optimized_windows(lat, lon, date_list):
 
 # --- Configuration ---
 root_folder = r"/vol/amfitrite"
-sat_test_folder = os.path.join(root_folder, "sat_data", "v4_2")
+sat_test_folder = os.path.join(root_folder, "sat_data", "v4_3")
 uids_processed_path = os.path.join(sat_test_folder, "uids_processed.xlsx")
 problematic_uids_path = os.path.join(sat_test_folder, "problematic_uids_3.csv")
 
