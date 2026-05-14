6c8761ef6319fae9456040a890b636dd01f7c4d8
Kostas Pikounis <kostas.pikounis@inlecomsystems.com>
2026-05-14 15:20:04 +0300

    Update class_separation_hist.py

--- OW_CNN/class_separation_hist.py
+++ OW_CNN/class_separation_hist.py
@@ -191,9 +191,9 @@ if __name__ == "__main__":
     #plot_path_name_prefix = "/home/kostas/AMFITRITE/IW_and_OW_CNN/bigearthnet_resnet18/class_separation"
     #architecture = "resnet18"
     
-    pth_path_name = "/home/kostas/AMFITRITE/IW_and_OW_CNN/bigearthnet_resnet18_focal_loss/resnet18_bigearth_focal_loss.pth"
+    pth_path_name = "/home/kostas/AMFITRITE/IW_and_OW_CNN/bigearthnet_resnet18_focal_loss_gamma_1/resnet18_bigearth_focal_loss_gamma_1.pth"
     csv_path_name = "/home/kostas/AMFITRITE/IW_and_OW_CNN/amfitrite_universal_split.csv"
-    plot_path_name_prefix = "/home/kostas/AMFITRITE/IW_and_OW_CNN/bigearthnet_resnet18_focal_loss/class_separation"
+    plot_path_name_prefix = "/home/kostas/AMFITRITE/IW_and_OW_CNN/bigearthnet_resnet18_focal_loss_gamma_1/class_separation"
     architecture = "resnet18"
 
 
