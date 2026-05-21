68ee1bd024fd32ac8f03bd337f7242f0f5f0862d
Kostas Pikounis <kostas.pikounis@inlecomsystems.com>
2026-05-21 14:44:12 +0300

    Update convert_ckpt_to_pth.py

--- OW_CNN/convert_ckpt_to_pth.py
+++ OW_CNN/convert_ckpt_to_pth.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     OUTPUT_PTH = "/home/kostas/AMFITRITE/IW_and_OW_CNN/bigearthnet_resnet18_focal_loss_gamma_1/resnet18_bigearth_focal_loss_gamma_1.pth"
     
     INPUT_CKPT = "/home/kostas/AMFITRITE/IW_and_OW_CNN/bigearthnet_resnet18/logs/version_0/checkpoints/best-hab-epoch=35-val_f1_macro=0.880.ckpt"
-    OUTPUT_PTH = "home/kostas/AMFITRITE/IW_and_OW_CNN/bigearthnet_resnet18/amfitrite_resnet18_bigearth_best_ep_35.pth"
+    OUTPUT_PTH = "/home/kostas/AMFITRITE/IW_and_OW_CNN/bigearthnet_resnet18/amfitrite_resnet18_bigearth_best_ep_35.pth"
 
     
     convert_ckpt_to_raw_pth(INPUT_CKPT, OUTPUT_PTH)
