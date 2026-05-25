b6b1f5c945b9127454a485548ec0c6f38b675870
Kostas Pikounis <kostas.pikounis@inlecomsystems.com>
2026-05-25 16:23:35 +0300

    Update separation_plots_unversal_resnet18.py

--- paper/separation_plots_unversal_resnet18.py
+++ paper/separation_plots_unversal_resnet18.py
@@ -139,6 +139,7 @@ def plot_ieee_class_separation(data_dict, split_name, output_path):
     # Log scale is mandatory for these density plots to see the overlap
     ax.set_yscale('log')
     ax.set_xlim(-0.02, 1.02)
+    ax.set_ylim(0.05, 10)
     
     # IEEE Labels using full words, strictly 8pt
     ax.set_xlabel("Prediction Score", fontsize=8, labelpad=1)
@@ -171,8 +172,8 @@ if __name__ == "__main__":
     csv_path_name = "/home/kostas/AMFITRITE/IW_and_OW_CNN/amfitrite_universal_split.csv"
     #plot_path_name_prefix = "/home/kostas/AMFITRITE/IW_and_OW_CNN/bigearthnet_resnet18_focal_loss_gamma_2/for_paper/class_separation"
     
-    pth_path_name = "/home/kostas/AMFITRITE/IW_and_OW_CNN/bigearthnet_resnet18/amfitrite_resnet18_bigearth_best_ep_35.pth"
-    plot_path_name_prefix = "/home/kostas/AMFITRITE/IW_and_OW_CNN/bigearthnet_resnet18/for_paper"
+    #pth_path_name = "/home/kostas/AMFITRITE/IW_and_OW_CNN/bigearthnet_resnet18/amfitrite_resnet18_bigearth_best_ep_35.pth"
+    #plot_path_name_prefix = "/home/kostas/AMFITRITE/IW_and_OW_CNN/bigearthnet_resnet18/for_paper"
     
     
     architecture = "resnet18"
